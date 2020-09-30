a0b25eb44fe494a7d5052d2ac329ef596495650d
wwperry94 <wwperry94@gmail.com>
2020-09-30 10:39:45 -0500

    sharpened

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var friends = ['Josh, ', 'Jake, ', 'Garrett, ', 'Will, ', 'Carlton, '];
 var weapons = ['pencil ', 'machete ', 'candlestick ', 'ladel ', 'firepoker ', 'letter-opener ', 'wine bottle ', 'piano wire ', 'ink quill ', 'gun ', 'club ', 'mallet ', 'frying pan ', 'box-cutter ', 'glass ', 'toaster ', 'sword ', 'statue ', 'shoe ', 'vase ']
 var places = ['bathroom!', 'parlor!', 'study!', 'wine-cellar!', 'bedroom!', 'garage!', 'attic!', 'stables!', 'kitchen!', 'closet!'];
-for (var i = 1; i < 101; i++) {
-    createH3(i);
+for (var i = 1; i < 101; i++) { 
+    createH3(i); 
 };
 function createH3(count) {
     var h3 = document.createElement('h3');
     h3.setAttribute("id", count);
     h3.innerHTML = `Accusation ${count}`;
     document.body.appendChild(h3);
-    h3.addEventListener('click', () => alert("I accuse " + friends[i % friends.length] + "with the " + weapons[i % weapons.length] + "in the " + places[i % places.length]));
+    h3.addEventListener('click', () => alert("I accuse " + friends[count % friends.length] + "with the " + weapons[count % weapons.length] + "in the " + places[count % places.length]));
 };
