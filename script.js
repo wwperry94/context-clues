036ace65ecd9944b55bee8a9ebb277b19d927e74
wwperry94 <wwperry94@gmail.com>
2020-09-30 11:22:28 -0500

    sharpened

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var friends = ['Josh, ', 'Jake, ', 'Garrett, ', 'Will, ', 'Carlton, '];
 var weapons = ['pencil ', 'machete ', 'candlestick ', 'ladel ', 'firepoker ', 'letter-opener ', 'wine bottle ', 'piano wire ', 'ink quill ', 'gun ', 'club ', 'mallet ', 'frying pan ', 'box-cutter ', 'glass ', 'toaster ', 'sword ', 'statue ', 'shoe ', 'vase ']
 var places = ['bathroom!', 'parlor!', 'study!', 'wine-cellar!', 'bedroom!', 'garage!', 'attic!', 'stables!', 'kitchen!', 'closet!'];
-for (var i = 1; i < 101; i++) { 
-    createH3(i); 
+for (var i = 1; i < 101; i++) {
+    createH3(i);
 };
 function createH3(count) {
     var h3 = document.createElement('h3');
